52d69c1a7216cb169d180b3b03f4ff92e46f080c
Sebastian42-5 <sebassotosandrea@gmail.com>
2025-06-28 13:15:21 -0400

    starting concert mini project

---
+++ concert_finder.py
@@ -0,0 +1,8 @@
+import requests
+
+my_key = 'bandsintown_api_key'
+artist = input('Enter the artist/band that you want to check: ')
+city = input('Enter the city: ')
+
+def find_concert(artist, city):
+    pass
